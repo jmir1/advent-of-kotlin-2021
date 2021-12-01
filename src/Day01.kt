d4712e899beb9b60ecb7ed89e9cb347df8497c95
jmir1 <jhmiramon@gmail.com>
2021-12-01 23:33:50 +0100

    day one

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,30 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val intList = input.map { it.toInt() }
+        var last = intList.first()
+        var result = 0
+        intList.forEach {
+            if (it > last) result++
+            last = it
+        }
+        return result
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val intList = input.map { it.toInt() }
+        var last = intList[0] + intList[1] + intList[2]
+        var result = 0
+        intList.forEachIndexed { index, it ->
+            if (index > 2) {
+                val sum = intList[index - 2] + intList[index - 1] + it
+                if (sum > last) result++
+                last = sum
+            }
+        }
+        return result
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    val input = readInput("Day01")
+    val input = readInput("input-day01")
     println(part1(input))
     println(part2(input))
 }
